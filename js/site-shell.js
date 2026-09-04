902e138eac695e511468c8850638a88e577ff61d
jestonibbaguio-max <jestoni.b.baguio@gmail.com>
2026-09-05 00:38:03 +0800

    Polish responsive shared site layout

--- js/site-shell.js
+++ js/site-shell.js
@@ -11,7 +11,6 @@
         <input id="search-input" type="search" placeholder="Search stores" aria-label="Search stores">
         <div class="search-results" id="search-results" role="listbox" aria-label="Search results"></div>
       </form>
-      <button class="create-store-button" id="open-store-modal" type="button">Create a store</button>
       <a href="#" class="nav-cart" id="nav-cart" aria-label="Shopping bag">
         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor">
           <path stroke-linecap="round" stroke-linejoin="round" d="M15.75 10.5V6a3.75 3.75 0 1 0-7.5 0v4.5m11.356-1.993 1.263 12c.07.665-.45 1.243-1.119 1.243H4.25a1.125 1.125 0 0 1-1.12-1.243l1.264-12A1.125 1.125 0 0 1 5.513 7.5h12.974A1.125 1.125 0 0 1 19.606 8.507Z"/>
@@ -28,11 +27,9 @@
           <a href="/privacy.html">Privacy Policy</a>
           <a href="/terms.html">Terms and Conditions</a>
         </div>
-        <div class="footer-socials">
-          <a href="#" aria-label="Instagram">Instagram</a>
-          <a href="#" aria-label="Twitter">Twitter</a>
-          <a href="#" aria-label="Facebook">Facebook</a>
-        </div>
+        <a class="footer-logo" href="/index.html" aria-label="BusyBagz home">
+          <img src="https://res.cloudinary.com/sjnrfmjm/image/upload/v1788502910/busyBagz.png" alt="BusyBagz logo">
+        </a>
       </div>
     </footer>`;
 
